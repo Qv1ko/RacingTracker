264056e0a7daad1cd443b383f585a29ff0aa7c03
Qv1ko <vicgarmur947@gmail.com>
2025-02-26 17:16:56 +0100

    Cambio de tipo de dato de la posición y correción de los nombres de los campos

--- db/create.sql
+++ db/create.sql
@@ -28,13 +28,13 @@ CREATE TABLE IF NOT EXISTS races (
 
 CREATE TABLE participations (
   `id` BIGINT AUTO_INCREMENT NOT NULL,
-  `driver_id` BIGINT NOT NULL,
-  `team_id` BIGINT DEFAULT NULL,
-  `race_id` BIGINT NOT NULL,
-  `position` TINYINT UNSIGNED NOT NULL,
-  `driver_points` DECIMAL(8,3) NOT NULL DEFAULT 0,
+  `driverId` BIGINT NOT NULL,
+  `teamId` BIGINT DEFAULT NULL,
+  `raceId` BIGINT NOT NULL,
+  `position` VARCHAR(4) NOT NULL,
+  `driverPoints` DECIMAL(8,3) NOT NULL DEFAULT 0,
   `uncertainty` DECIMAL(8,3) NOT NULL DEFAULT 0,
-  `team_points` DECIMAL(8,3) DEFAULT NULL,
+  `teamPoints` DECIMAL(8,3) DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
@@ -42,19 +42,19 @@ CREATE TABLE participations (
 ALTER TABLE drivers ADD UNIQUE (`name`, `surname`);
 ALTER TABLE teams ADD UNIQUE (`name`);
 ALTER TABLE races ADD UNIQUE (`name`, `date`);
-ALTER TABLE participations ADD UNIQUE (`driver_id`, `race_id`);
+ALTER TABLE participations ADD UNIQUE (`driverId`, `raceId`);
 
 -- Foreign keys
 ALTER TABLE participations 
-  ADD CONSTRAINT `fk_participations_driver` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `fk_participations_team` FOREIGN KEY (`team_id`) REFERENCES `teams` (`id`) ON DELETE SET NULL,
-  ADD CONSTRAINT `fk_participations_race` FOREIGN KEY (`race_id`) REFERENCES `races` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `fk_participations_driver` FOREIGN KEY (`driverId`) REFERENCES `drivers` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_participations_team` FOREIGN KEY (`teamId`) REFERENCES `teams` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `fk_participations_race` FOREIGN KEY (`raceId`) REFERENCES `races` (`id`) ON DELETE CASCADE;
 
 -- Indexes
 CREATE INDEX idx_drivers_nationality ON drivers (`nationality`);
 CREATE INDEX idx_teams_nationality ON teams (`nationality`);
 CREATE INDEX idx_races_date ON races (`date`);
-CREATE INDEX idx_participations_driver ON participations (`driver_id`);
-CREATE INDEX idx_participations_race ON participations (`race_id`);
-CREATE INDEX idx_participations_team ON participations (`team_id`);
+CREATE INDEX idx_participations_driver ON participations (`driverId`);
+CREATE INDEX idx_participations_race ON participations (`raceId`);
+CREATE INDEX idx_participations_team ON participations (`teamId`);
 CREATE INDEX idx_participations_position ON participations (`position`);
