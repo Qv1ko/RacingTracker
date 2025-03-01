32920382ea94880482d280407657dda01edff758
Qv1ko <vicgarmur947@gmail.com>
2025-03-01 19:40:01 +0100

    Eliminación del campo color de la tabla teams, cambio de la codificación de la base de datos y corrección del diagrama entidad-relación y relacional

--- db/create.sql
+++ db/create.sql
@@ -1,30 +1,34 @@
 -- Database
-CREATE DATABASE IF NOT EXISTS racingtracker;
+DROP DATABASE IF EXISTS racingtracker;
+CREATE DATABASE racingtracker;
+
 USE racingtracker;
 
+SET NAMES latin1;
+SET CHARACTER SET latin1_swedish_ci;
+
 -- Tables
-CREATE TABLE IF NOT EXISTS drivers (
+CREATE TABLE drivers (
   `id` BIGINT AUTO_INCREMENT NOT NULL,
   `name` VARCHAR(50) NOT NULL,
   `surname` VARCHAR(100) NOT NULL,
   `nationality` VARCHAR(100),
   PRIMARY KEY (`id`)
-);
+) ENGINE=INNODB CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 
-CREATE TABLE IF NOT EXISTS teams (
+CREATE TABLE teams (
   `id` BIGINT AUTO_INCREMENT NOT NULL,
   `name` VARCHAR(100) NOT NULL,
   `nationality` VARCHAR(100),
-  `color` CHAR(7),
   PRIMARY KEY (`id`)
-);
+) ENGINE=INNODB CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 
-CREATE TABLE IF NOT EXISTS races (
+CREATE TABLE races (
   `id` BIGINT AUTO_INCREMENT NOT NULL,
   `name` VARCHAR(100) NOT NULL,
   `date` DATE NOT NULL,
   PRIMARY KEY (`id`)
-);
+) ENGINE=INNODB CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 
 CREATE TABLE participations (
   `id` BIGINT AUTO_INCREMENT NOT NULL,
@@ -36,7 +40,7 @@ CREATE TABLE participations (
   `uncertainty` DECIMAL(8,3) NOT NULL DEFAULT 0,
   `teamPoints` DECIMAL(8,3) DEFAULT NULL,
   PRIMARY KEY (`id`)
-);
+) ENGINE=INNODB CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 
 -- Unique constraints
 ALTER TABLE drivers ADD UNIQUE (`name`, `surname`);
@@ -58,3 +62,41 @@ CREATE INDEX idx_participations_driver ON participations (`driverId`);
 CREATE INDEX idx_participations_race ON participations (`raceId`);
 CREATE INDEX idx_participations_team ON participations (`teamId`);
 CREATE INDEX idx_participations_position ON participations (`position`);
+
+-- Inserts
+INSERT INTO drivers (name, surname, nationality) VALUES
+('Jean', 'Alesi', 'French'),
+('Rubens', 'Barrichello', 'Brazilian'),
+('Luciano', 'Burti', 'Brazilian'),
+('Jenson', 'Button', 'British'),
+('David', 'Coulthard', 'British'),
+('Pedro', 'de la Rosa', 'Spanish'),
+('Giancarlo', 'Fisichella', 'Italian'),
+('Heinz-Harald', 'Frentzen', 'German'),
+('Marc', 'Gené', 'Spanish'),
+('Mika', 'Häkkinen', 'Finnish'),
+('Nick', 'Heidfeld', 'German'),
+('Johnny', 'Herbert', 'British'),
+('Eddie', 'Irvine', 'British'),
+('Gastón', 'Mazzacane', 'Argentine'),
+('Mika', 'Salo', 'Finnish'),
+('Michael', 'Schumacher', 'German'),
+('Ralf', 'Schumacher', 'German'),
+('Jarno', 'Trulli', 'Italian'),
+('Jos', 'Verstappen', 'Dutch'),
+('Jacques', 'Villeneuve', 'Canadian'),
+('Alexander', 'Wurz', 'Austrian'),
+('Ricardo', 'Zonta', 'Brazilian');
+
+INSERT INTO teams (name, nationality) VALUES
+('Arrows', 'British'),
+('BAR', 'British'),
+('Benetton', 'Italian'),
+('Ferrari', 'Italian'),
+('Jaguar', 'British'),
+('Jordan', 'British'),
+('McLaren', 'British'),
+('Minardi', 'Italian'),
+('Prost', 'French'),
+('Sauber', 'Swiss'),
+('Williams', 'British');
