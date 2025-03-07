9418e67f6d366232f51068415c4915fecf0ce92f
Qv1ko <vicgarmur947@gmail.com>
2025-03-07 20:06:14 +0100

    Mejorados los estilos del header y footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-800 text-white p-4 mt-8">
-      <div className="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} Mi Aplicación. Todos los derechos reservados.</p>
+    <footer className="bg-transparent text-gray mt-8">
+      <hr className="w-48 h-1 mx-auto bg-gray-400 border-0 dark:bg-gray-700" />
+      <div className="container mx-auto text-center p-4">
+        <p>powered by <a href="https://github.com/Qv1ko" target="_blank" rel="noopener noreferrer">Qv1ko</a></p>
       </div>
     </footer>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,23 +3,26 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-blue-600 text-white p-4">
+    <header className="sticky top-0 bg-black text-white p-4 drop-shadow-lg">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-xl font-bold">
           <Link href="/">
-            Mi Aplicación
+            RacingTracker
           </Link>
         </h1>
         <nav>
           <ul className="flex space-x-4">
             <li>
-              <Link href="/">Home</Link>
+              <Link href="/">HOME</Link>
             </li>
             <li>
-              <Link href="/drivers">Drivers</Link>
+              <Link href="/drivers">DRIVERS</Link>
             </li>
             <li>
-              <Link href="/teams">Teams</Link>
+              <Link href="/teams">TEAMS</Link>
+            </li>
+            <li>
+              <Link href="/teams">SEASONS</Link>
             </li>
           </ul>
         </nav>
