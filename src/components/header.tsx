00d21c4d6b6c5a5e52433fd826123040f32ac095
Qv1ko <vicgarmur947@gmail.com>
2025-03-22 18:29:25 +0100

    Mejorado el header y añadido favicon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
@@ -5,24 +6,25 @@ const Header: React.FC = () => {
   return (
     <header className="sticky top-0 bg-black text-white p-4 drop-shadow-lg">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold">
-          <Link href="/">
-            RacingTracker
-          </Link>
-        </h1>
+        <Link href="/">
+          <Image src="/racingtracker.svg" alt="RacingTracker logo" width={96} height={48} priority />
+        </Link>
         <nav>
-          <ul className="flex space-x-4">
+          <ul className="flex space-x-4 font-semibold">
             <li>
               <Link href="/">HOME</Link>
+            </li>
+            <li>
+              <Link href="/seasons">SEASONS</Link>
+            </li>
+            <li>
+              <Link href="/races">RACES</Link>
             </li>
             <li>
               <Link href="/drivers">DRIVERS</Link>
             </li>
             <li>
               <Link href="/teams">TEAMS</Link>
-            </li>
-            <li>
-              <Link href="/teams">SEASONS</Link>
             </li>
           </ul>
         </nav>
