e3bdd1f1d0dcec5cb2b5b759330f439da9d08914
Qv1ko <vicgarmur947@gmail.com>
2025-03-29 21:38:49 +0100

    Modificado el nombre del title

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,10 +5,10 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'RacingTracker';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => title.length > 0 ? `${title} - ${appName}` : appName,
     resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
