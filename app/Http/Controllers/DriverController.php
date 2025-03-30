0f0de3fad985cae417a87e83239fc1a3822f80ac
Qv1ko <vicgarmur947@gmail.com>
2025-03-30 21:57:59 +0200

    Vista de index del modelo de Drivers

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -3,8 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class DriverController extends Controller
 {
-    //
+    public function index(Request $req)
+    {
+        return Inertia::render('drivers/index');
+    }
+
+    public function show(Request $req)
+    {
+        return Inertia::render('drivers/show');
+    }
+
+    public function create()
+    {
+        return Inertia::render('drivers/create');
+    }
+
+    public function store(Request $req)
+    {
+        return Inertia::render('drivers/store');
+    }
+
+    public function edit(Request $req)
+    {
+        return Inertia::render('drivers/edit');
+    }
+
+    public function update(Request $req)
+    {
+        return Inertia::render('drivers/update');
+    }
+
+    public function destroy(Request $req)
+    {
+        return Inertia::render('drivers/destroy');
+    }
 }
