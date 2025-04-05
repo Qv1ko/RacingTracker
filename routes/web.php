50ab3e9ee6c2830d902612c0c7128bbe5bf0f99a
Qv1ko <vicgarmur947@gmail.com>
2025-04-05 23:14:44 +0200

    Simplificadas las rutas

--- routes/web.php
+++ routes/web.php
@@ -10,27 +10,14 @@ Route::get('/', function () {
     return Inertia::render('home');
 })->name('home');
 
-Route::get('/drivers', [DriverController::class, 'index'])->name('drivers.index');
-Route::get('/drivers/{driver}', [DriverController::class, 'show'])->name('drivers.show');
-
-Route::get('/teams', [TeamController::class, 'index'])->name('teams.index');
-Route::get('/teams/{team}', [TeamController::class, 'show'])->name('teams.show');
-
-Route::get('/races', [RaceController::class, 'index'])->name('races.index');
-Route::get('/races/{race}', [RaceController::class, 'show'])->name('races.show');
+Route::resource('drivers', DriverController::class)->only(['index', 'show']);
+Route::resource('teams', TeamController::class)->only(['index', 'show']);
+Route::resource('races', RaceController::class)->only(['index', 'show']);
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::post('drivers', [DriverController::class, 'store'])->name('drivers.store');
-    Route::put('drivers/{driver}', [DriverController::class, 'update'])->name('drivers.update');
-    Route::delete('drivers/{driver}', [DriverController::class, 'destroy'])->name('drivers.destroy');
-
-    Route::post('teams', [TeamController::class, 'store'])->name('teams.store');
-    Route::put('teams/{team}', [TeamController::class, 'update'])->name('teams.update');
-    Route::delete('teams/{team}', [TeamController::class, 'destroy'])->name('teams.destroy');
-
-    Route::post('races', [RaceController::class, 'strore'])->name('races.store');
-    Route::put('races/{race}', [RaceController::class, 'update'])->name('races.update');
-    Route::delete('races/{race}', [RaceController::class, 'destroy'])->name('races.destroy');
+    Route::resource('drivers', DriverController::class)->only(['create', 'store', 'edit', 'update', 'destroy']);
+    Route::resource('teams', TeamController::class)->only(['create', 'store', 'edit', 'update', 'destroy']);
+    Route::resource('races', RaceController::class)->only(['create', 'store', 'edit', 'update', 'destroy']);
 });
 
 require __DIR__ . '/settings.php';
