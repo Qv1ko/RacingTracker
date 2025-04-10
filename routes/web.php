4a4f2f3eca6d4a5b8941c6b7a886ee8992102772
Qv1ko <vicgarmur947@gmail.com>
2025-04-10 21:47:42 +0200

    Cambiadas las rutas para que la función show no de problemas de rutas

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,15 @@ Route::get('/', function () {
     return Inertia::render('home');
 })->name('home');
 
-Route::resource('drivers', DriverController::class)->only(['index', 'show']);
-Route::resource('teams', TeamController::class)->only(['index', 'show']);
-Route::resource('races', RaceController::class)->only(['index', 'show']);
-
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('drivers', DriverController::class)->only(['create', 'store', 'edit', 'update', 'destroy']);
     Route::resource('teams', TeamController::class)->only(['create', 'store', 'edit', 'update', 'destroy']);
     Route::resource('races', RaceController::class)->only(['create', 'store', 'edit', 'update', 'destroy']);
 });
+
+Route::resource('drivers', DriverController::class)->only(['index', 'show']);
+Route::resource('teams', TeamController::class)->only(['index', 'show']);
+Route::resource('races', RaceController::class)->only(['index', 'show']);
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
