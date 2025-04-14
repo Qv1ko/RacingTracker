a5165a2311feed8470f0546b5ea1e86ce773b1c3
Qv1ko <vicgarmur947@gmail.com>
2025-04-15 01:18:20 +0200

    Instalación y configuración de la tipografía

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -33,8 +33,7 @@
 
     <title inertia>{{ config('app.name', 'RacinTracker') }}</title>
 
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Open+Sans&display=swap" rel="stylesheet">
 
     @routes
     @viteReactRefresh
