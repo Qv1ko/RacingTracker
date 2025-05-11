2f08bba8d0a7ec291261de2a045c67574f90bea5
Qv1ko <vicgarmur947@gmail.com>
2025-05-11 21:45:47 +0200

    Mejorando el responsive de la tabla de información

--- resources/js/components/info-grid.tsx
+++ resources/js/components/info-grid.tsx
@@ -18,39 +18,27 @@ export default function InfoGrid({ data }: InfoGridProps) {
     const lastItem = isOdd ? data[data.length - 1] : null;
 
     return (
-        <div className={'overflow-hidden rounded-md border'}>
-            <div className="grid grid-cols-2">
-                <div className="border-r">
+        <div className="overflow-hidden rounded-md border">
+            <div className="grid grid-cols-1 md:grid-cols-2">
+                <div className="flex flex-col divide-y md:border-r">
                     {leftColumnItems.map((info, index) => (
-                        <div
-                            key={`left-${index}`}
-                            className={`flex items-center justify-between px-4 py-3 ${
-                                index < leftColumnItems.length - 1 || lastItem ? 'border-b' : ''
-                            }`}
-                        >
+                        <div key={`left-${index}`} className="flex items-center justify-between px-4 py-3">
                             <div className="font-medium">{info.key}</div>
                             <div className="text-right">{info.value}</div>
                         </div>
                     ))}
                 </div>
-
-                <div>
+                <div className="flex flex-col divide-y border-t md:border-t-0">
                     {rightColumnItems.map((info, index) => (
-                        <div
-                            key={`right-${index}`}
-                            className={`flex items-center justify-between px-4 py-3 ${
-                                index < rightColumnItems.length - 1 || lastItem ? 'border-b' : ''
-                            }`}
-                        >
+                        <div key={`right-${index}`} className="flex items-center justify-between px-4 py-3">
                             <div className="font-medium">{info.key}</div>
                             <div className="text-right">{info.value}</div>
                         </div>
                     ))}
                 </div>
             </div>
-
             {lastItem && (
-                <div className="flex items-center justify-between px-4 py-3">
+                <div className="flex items-center justify-between border-t px-4 py-3">
                     <div className="font-medium">{lastItem.key}</div>
                     <div className="text-right">{lastItem.value}</div>
                 </div>
