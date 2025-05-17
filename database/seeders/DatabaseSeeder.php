da801f1c24e79e6dc8c5fdbd888dfca94222d23b
Qv1ko <vicgarmur947@gmail.com>
2025-05-17 22:19:47 +0200

    Seeder para crear temporadas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,7 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call([
-            DriverSeeder::class,
-            TeamSeeder::class,
+            SeasonSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/SeasonSeeder.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Driver;
+use App\Models\Participation;
+use App\Models\Race;
+use App\Models\Team;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SeasonSeeder extends Seeder
+{
+    private const SEASONS = 25;
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $startYear = now()->year - self::SEASONS + 1;
+
+        for ($i = 0; $i < self::SEASONS; $i++) {
+            $seasonYear = $startYear + $i;
+
+            $this->updateDrivers(1, 30, 3);
+            $this->updateTeams(1, 10, 1);
+
+            $races = Race::factory()->fromYear($seasonYear)->count(rand(12, 24))->create();
+
+            $driverCount = Driver::where('status', true)->count();
+            $minParticipants = (int) round($driverCount * 0.9);
+
+            foreach ($races as $race) {
+                Participation::factory()->race($race->id)->count(rand($minParticipants, $driverCount))->create();
+            }
+        }
+    }
+
+    private function updateDrivers(int $retireCount, int $initialCreate, int $additionalCreate): void
+    {
+        $drivers = Driver::where('status', true)
+            ->inRandomOrder()
+            ->limit($retireCount)
+            ->get();
+
+        $drivers->each->update(['status' => false]);
+
+        $activeCount = Driver::where('status', true)->count();
+
+
+        Driver::factory()->count($activeCount === 0 ? $initialCreate : $additionalCreate)->create();
+    }
+
+    private function updateTeams(int $retireCount, int $initialCreate, int $additionalCreate): void
+    {
+        $teams = Team::where('status', true)
+            ->inRandomOrder()
+            ->limit($retireCount)
+            ->get();
+
+        $teams->each->update(['status' => false]);
+
+        $activeCount = Team::where('status', true)->count();
+
+        Team::factory()->count($activeCount === 0 ? $initialCreate : $additionalCreate)->create();
+    }
+}
