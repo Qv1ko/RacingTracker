75aa6a4ac7d7ef553a392c337dc8bedcb37c5857
Qv1ko <vicgarmur947@gmail.com>
2025-05-18 20:39:49 +0200

    Añadidos seeders de ejemplo comentados

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Driver;
+use App\Models\Team;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,7 +22,9 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call([
-            SeasonSeeder::class,
+            // SeasonSeeder::class,
+            // DriverSeeder::class,
+            // TeamSeeder::class,
         ]);
     }
 }
